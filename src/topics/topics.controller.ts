8061b4b8b398daf698f6971b2fff949ab2a2b008
Jem Suarez <jempoolsmz@gmail.com>
2023-12-13 18:01:11 -0500

    Adding 'Random Topics' feature

---
+++ src/topics/topics.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { TopicsService } from './topics.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+
+@Controller('topics')
+export class TopicsController {
+  constructor(private readonly topicsService: TopicsService) {}
+
+  @UseGuards(AuthGuard)
+  @Get()
+  findAll() {
+    return this.topicsService.findAll();
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('today')
+  findTodaysTopic() {
+    return this.topicsService.findTodaysTopic();
+  }
+}
